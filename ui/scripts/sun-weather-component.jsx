48107dea67f07e9ada3022e2e0a9d0b3926b5111
denny2006 <denny2006@gmail.com>
2016-09-08 22:00:02 +0300

    Using Axios instead of fetch() and utilizing ES6 classes

--- ui/scripts/sun-weather-component.jsx
+++ ui/scripts/sun-weather-component.jsx
@@ -1,47 +1,35 @@
 import React from 'react';
+import axios from 'axios';
 
-module.exports = React.createClass({
-  getInitialState: function() {
-    return {
+class SunWeatherComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       sunrise: undefined,
       sunset: undefined,
       temperature: undefined,
       requests: undefined
-    }
-  },
-  componentDidMount: function() {
-    let reactRef = this;
-    fetch('/data').then(function(response) {
-      return response.json();
-    }).then(function(json) {
-      reactRef.setState({
+    };
+  }
+  componentDidMount = () => {
+    axios.get('/data').then((response) => {
+      const json = response.data;
+      this.setState({
         sunrise: json.sunInfo.sunrise,
         sunset: json.sunInfo.sunset,
         temperature: json.temperature,
         requests: json.requests
       });
-    });
-  },
-  render: function() {
-    return <table>
-      <tbody>
-        <tr>
-          <td>Sunrise time</td>
-          <td>{this.state.sunrise}</td>
-        </tr>
-        <tr>
-          <td>Sunset time</td>
-          <td>{this.state.sunset}</td>
-        </tr>
-        <tr>
-          <td>Current temperature</td>
-          <td>{this.state.temperature}</td>
-        </tr>
-        <tr>
-          <td>Requests</td>
-          <td>{this.state.requests}</td>
-        </tr>
-      </tbody>
-    </table>
+    })
   }
-});
+  render = () => {
+    return <div>
+      <div>Sunrise time: {this.state.sunrise}</div>
+      <div>Sunset time: {this.state.sunset}</div>
+      <div>Current temperature: {this.state.temperature}</div>
+      <div>Requests: {this.state.requests}</div>
+    </div>
+  }
+}
+
+export default SunWeatherComponent;
